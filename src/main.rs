d9b33feb148541a8761445352212538c3488657e
Axel Garcia <axelegar@gmail.com>
2022-08-06 16:48:27 -0700

    learn about snake case in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bdays::HolidayCalendar;
+use chrono::NaiveDate;
 fn main() {
+    let cal = bdays::calendars::WeekendsOnly;
     println!("This is the start of dayOff programming!");
+    let d0 = NaiveDate::from_ymd(2022, 2, 1);
+    let check = cal.is_holiday(d0);
+    assert_eq!(check, false);
+    println!("{}", check);
 }
