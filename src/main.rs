4e49f966f3931ca4017aeec62c58450425843a22
Axel Garcia <axelegar@gmail.com>
2022-08-06 22:23:33 -0700

    Build holiday list for weekdays.
    Signed-off-by: Axel Garcia <axelegar@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-// use bdays::HolidayCalendar;
+use bdays::HolidayCalendar;
 use chrono::NaiveDate;
-use std::io::stdin;
+use std::{array, io::stdin, vec};
 fn main() {
-    // let cal = bdays::calendars::WeekendsOnly;
     println!("This is the start of dayOff programming!");
-    println!("Please enter the year you want number of days for: ");
+    // User input from cmdline
+    println!("What year are you asking for?");
     let mut user_year_input = String::new();
     stdin()
         .read_line(&mut user_year_input)
@@ -12,13 +12,27 @@ fn main() {
     let year: i32 = user_year_input
         .trim()
         .parse()
-        .expect("Input not an integer");
-    println!("{:?}", days_in_year(year))
+        .expect("Input not an integer.");
+    let days_in_year: i32 = days_in_year(year);
+    build_holiday_list(days_in_year, year);
 }
-
-fn days_in_year(year: i32) -> i64 {
+// Build list of holidays weekends are given off already
+fn build_holiday_list(days_in_year: i32, year: i32) {
+    let cal = bdays::calendars::us::USSettlement;
+    let mut vector: Vec<NaiveDate> = Vec::new();
+    let mut iterator: i32 = 1;
+    while iterator < days_in_year {
+        if cal.is_holiday(NaiveDate::from_yo(year, iterator.try_into().unwrap())) {
+            vector.push(NaiveDate::from_yo(year, iterator.try_into().unwrap()));
+        }
+        iterator += 1;
+    }
+    println!("{:?}", vector);
+}
+// Get the number of days for a given year
+fn days_in_year(year: i32) -> i32 {
     let since = NaiveDate::signed_duration_since;
     let from_ymd = NaiveDate::from_ymd;
     let days = (since(from_ymd(year, 1, 1), from_ymd(year + 1, 1, 1))).num_days();
-    return days.abs();
+    return days.abs().try_into().unwrap();
 }
