fd51d80f4ea5b27c4c1cbc1a041a6308c9368b5d
Axel Garcia <axelegar@gmail.com>
2022-08-06 19:43:44 -0700

    Learning gettin input from cmd line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 // use bdays::HolidayCalendar;
 use chrono::NaiveDate;
+use std::io::stdin;
 fn main() {
     // let cal = bdays::calendars::WeekendsOnly;
     println!("This is the start of dayOff programming!");
-    println!("{:?}", days_in_year(2024))
+    println!("Please enter the year you want number of days for: ");
+    let mut user_year_input = String::new();
+    stdin()
+        .read_line(&mut user_year_input)
+        .expect("Failed to read line.");
+    let year: i32 = user_year_input
+        .trim()
+        .parse()
+        .expect("Input not an integer");
+    println!("{:?}", days_in_year(year))
 }
 
 fn days_in_year(year: i32) -> i64 {
